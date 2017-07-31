fcc53896e152ce3e0ae9b3e39151ae89d2a2fc38
dflay <dflay27@gmail.com>
2017-07-31 15:58:09 -0500

    Added the SG382 class

---
+++ hardware-lib/sg382-interface/include/SG382.hh
@@ -0,0 +1,36 @@
+#ifndef SG382_H
+#define SG382_H
+
+#include <cstdlib>
+#include <string> 
+
+// #include "CommDriver.hh"
+#include "Device.hh"
+#include "SG382Interface.hh"
+
+class SG382: public Device{
+
+   public:
+      SG382();
+      ~SG382();
+
+      int SetFrequency(double freq);
+      int SetNTypeAmp(double amp);
+      int SetBNCAmp(double amp);
+      int SetBNCOutputState(int flag);
+      int SetNTypeOutputState(int flag);
+      int SetModulationState(int flag);
+      int SetModulationRate(double freq);
+      int SetModulationFunction(int flag);
+
+      int GetBNCOutputState(int &state);
+      int GetNTypeOutputState(int &state);
+      int GetBNCAmplitude(double &amp);
+      int GetNTypeAmplitude(double &amp);
+      int GetFrequency(double &freq);
+
+      int CheckErrors(char *err_msg);
+
+};
+
+#endif 
---
+++ hardware-lib/sg382-interface/src/SG382.cxx
@@ -0,0 +1,146 @@
+#include "SG382.hh"
+//______________________________________________________________________________
+SG382::SG382(){
+   fName = "SRS SG382"; 
+}
+//______________________________________________________________________________
+SG382::~SG382(){
+
+}
+//______________________________________________________________________________
+int SG382::SetFrequency(double freq){
+   char cmd[100];
+   sprintf(cmd, "FREQ %.7lf\n",freq);
+   int rc = CommDriver::write_cmd(fProtocol,fHandle,cmd);
+   return rc;
+}
+//______________________________________________________________________________
+int SG382::SetNTypeAmp(double amp){
+   char cmd[100];
+   sprintf(cmd, "AMPR %.7lf\n",amp);
+   int rc = CommDriver::write_cmd(fProtocol,fHandle,cmd);
+   return rc;
+}
+//______________________________________________________________________________
+int SG382::SetBNCAmp(double amp){
+   char cmd[100];
+   sprintf(cmd, "AMPL %.7lf\n",amp);
+   int rc = CommDriver::write_cmd(fProtocol,fHandle,cmd);
+   return rc;
+}
+//______________________________________________________________________________
+int SG382::SetBNCOutputState(int flag){
+   int rc=-1;
+   char cmd[100];
+   if(flag==SG382Interface::kDISABLE || flag==SG382Interface::kENABLE){
+      sprintf(cmd,"ENBL%d\n",flag);
+      rc = CommDriver::write_cmd(fProtocol,fHandle,cmd);
+   }else{
+      printf("[SG382::SetBNCOutputState]: ERROR: Invalid flag '%d' passed.\n",flag);
+   }
+   return rc;
+}
+//______________________________________________________________________________
+int SG382::SetNTypeOutputState(int flag){
+   int rc=-1;
+   char cmd[100];
+   if(flag==SG382Interface::kDISABLE || flag==SG382Interface::kENABLE){
+      sprintf(cmd,"ENBR%d\n",flag);
+      rc = CommDriver::write_cmd(fProtocol,fHandle,cmd);
+   }else{
+      printf("[SG382::SetBNCOutputState]: ERROR: Invalid flag '%d' passed.\n",flag);
+   }
+   return rc;
+}
+//______________________________________________________________________________
+int SG382::SetModulationState(int flag){
+   int rc=-1;
+   char cmd[100];
+   if(flag==SG382Interface::kDISABLE || flag==SG382Interface::kENABLE){
+      sprintf(cmd,"MODL%d\n",flag);
+      rc = CommDriver::write_cmd(fProtocol,fHandle,cmd);
+   }else{
+      printf("[SG382::SetModulationState]: ERROR: Invalid flag '%d' passed.\n",flag);
+   }
+   return rc;
+}
+//______________________________________________________________________________
+int SG382::SetModulationFunction(int flag){
+   int rc=-1;
+   char cmd[100];
+   if(flag==SG382Interface::kSINE   || flag==SG382Interface::kRAMP  || flag==SG382Interface::kTRIANGLE ||
+      flag==SG382Interface::kSQUARE || flag==SG382Interface::kNOISE || flag==SG382Interface::kEXTERNAL){
+      sprintf(cmd,"MFNC%d\n",flag);
+      rc = CommDriver::write_cmd(fProtocol,fHandle,cmd);
+   }else{
+      printf("[SG382::SetModulationFunction]: ERROR: Invalid flag passed.\n");
+   }
+   return rc;
+}
+//______________________________________________________________________________
+int SG382::SetModulationRate(double freq){
+   char cmd[100];
+   sprintf(cmd,"RATE %.14lf\n",freq);
+   int rc = CommDriver::write_cmd(fProtocol,fHandle,cmd);
+   return rc;
+}
+//______________________________________________________________________________
+int SG382::GetBNCOutputState(int &state){
+   char cmd[100],response[100];
+   sprintf(cmd,"ENBL?\n");
+   int rc = CommDriver::query(fProtocol,fHandle,cmd,response);
+   state = atoi(response);
+   return rc;
+}
+//______________________________________________________________________________
+int SG382::GetNTypeOutputState(int &state){
+   char cmd[100],response[100];
+   sprintf(cmd,"ENBR?\n");
+   int rc = CommDriver::query(fProtocol,fHandle,cmd,response);
+   state = atoi(response);
+   return rc;
+}
+//______________________________________________________________________________
+int SG382::GetBNCAmplitude(double &amp){
+   char cmd[100],response[100];
+   sprintf(cmd,"AMPL?\n");
+   int rc = CommDriver::query(fProtocol,fHandle,cmd,response);
+   amp = atof(response);
+   return rc;
+}
+//______________________________________________________________________________
+int SG382::GetNTypeAmplitude(double &amp){
+   char cmd[100],response[100];
+   sprintf(cmd,"AMPR?\n");
+   int rc = CommDriver::query(fProtocol,fHandle,cmd,response);
+   amp = atof(response);
+   return rc;
+}
+//______________________________________________________________________________
+int SG382::GetFrequency(double &freq){
+   char cmd[100],response[100];
+   sprintf(cmd,"FREQ?\n");
+   int rc = CommDriver::query(fProtocol,fHandle,cmd,response);
+   freq = atof(response);
+   return rc;
+}
+//______________________________________________________________________________
+int SG382::CheckErrors(char *err_msg){
+   const int SIZE = 512;
+   char cmd[SIZE],response[SIZE];
+   sprintf(cmd,"LERR?\n");
+   int rc = CommDriver::query(fProtocol,fHandle,cmd,response);
+   // parse the string; it's going to be an error code and a message
+   std::istringstream ss(response);
+   std::string token,entry[2];
+   int i=0;
+   while( std::getline(ss,token,',') ){
+      entry[i] = token;
+      i++;
+   }
+   // now return the data 
+   rc = atoi(entry[0].c_str());         // zeroth entry is the error code
+   strcpy( err_msg,entry[1].c_str() );  // copy the message to the buffer 
+   return rc;
+}
+
