163f5c78bb1eac3e818772ec9a89beeef1488b6e
dflay <dflay27@gmail.com>
2018-10-22 17:40:25 -0500

    Added the bare bones of the SIS3302, SIS3316 classes

---
+++ hardware-lib/sis-interface/include/SIS3302.hh
@@ -0,0 +1,16 @@
+#ifndef SIS3302_H
+#define SIS3302_H 
+
+// implementation of SIS3302 class
+
+class SIS3302: public SISBase, public Device { 
+
+   private: 
+
+   public:
+      SIS3302();
+      ~SIS3302():
+
+}; 
+
+#endif 
---
+++ hardware-lib/sis-interface/include/SIS3316.hh
@@ -0,0 +1,16 @@
+#ifndef SIS3316_H
+#define SIS3316_H 
+
+// implementation of SIS3316 class
+
+class SIS3316: public SISBase, public Device { 
+
+   private: 
+
+   public:
+      SIS3316();
+      ~SIS3316():
+
+}; 
+
+#endif 
